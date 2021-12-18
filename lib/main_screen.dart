cdb7da17fe18d7782df74b2e7f9a2ed6b441c66e
Ramesh M S <msramesh2k1@gmail.com>
2021-12-18 07:18:52 +0530

    feat : Custom Loader

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MainScreen extends StatefulWidget {
-  const MainScreen({ Key? key }) : super(key: key);
+  const MainScreen({Key? key}) : super(key: key);
 
   @override
   _MainScreenState createState() => _MainScreenState();
@@ -10,8 +11,15 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          color: Colors.black,
+          child: Center(
+            child: Image.asset("assets/images/logo3.gif"),
+          ),
+        ),
+      ),
     );
   }
 }
