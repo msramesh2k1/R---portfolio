c97f27929636d353dbbb49faf4e267e636cc2004
Ramesh M S <msramesh2k1@gmail.com>
2021-12-19 07:16:49 +0530

    style : responsive main screen

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 import 'package:r_portfolio/second_screen.dart';
@@ -15,54 +14,43 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          color: Colors.black,
-          height: MediaQuery.of(context).size.height,
-          child: Center(
-            child: PageView(
-              controller: pageController,
-              scrollDirection: Axis.vertical,
-              children: [
-                Stack(
-                  children: [
-                    Container(
-                      height: MediaQuery.of(context).size.height,
-                      width: MediaQuery.of(context).size.width,
-                      color: Colors.black,
-                      child: Hero(
-                        tag: "logo",
-                        child: Image.asset(
-                          "assets/images/logo5.png",
-                          height: 600,
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      bottom: 20,
-                      right: 40,
-                      child: GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                PageRouteBuilder(
-                                    transitionDuration:
-                                        const Duration(seconds: 2),
-                                    pageBuilder: (_, __, ___) =>
-                                        const SecondScreen()));
-                          },
-                          child: RotationTransition(
-                            turns: const AlwaysStoppedAnimation(270 / 360),
-                            child: Lottie.asset('assets/images/mouse.json',
-                                height: 50),
-                          )),
-                    )
-                  ],
+      body: Container(
+        color: Colors.black,
+        height: MediaQuery.of(context).size.height,
+        child: Center(
+          child: Stack(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                color: Colors.black,
+                child: Hero(
+                  tag: "logo",
+                  child: Image.asset(
+                    "assets/images/logo5.png",
+                    height: 600,
+                  ),
                 ),
-
-                //page2
-              ],
-            ),
+              ),
+              Positioned(
+                bottom: 20,
+                right: 40,
+                child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          PageRouteBuilder(
+                              transitionDuration: const Duration(seconds: 2),
+                              pageBuilder: (_, __, ___) =>
+                                  const SecondScreen()));
+                    },
+                    child: RotationTransition(
+                      turns: const AlwaysStoppedAnimation(270 / 360),
+                      child:
+                          Lottie.asset('assets/images/mouse.json', height: 50),
+                    )),
+              )
+            ],
           ),
         ),
       ),
